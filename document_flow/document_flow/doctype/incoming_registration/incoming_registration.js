c2931a67e3f40e990bf136acc12666727924839d
ZhuzhginNA <zhuzhginna@gmail.com>
2024-12-27 13:02:53 +0300

    initial commit

---
+++ document_flow/document_flow/doctype/incoming_registration/incoming_registration.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, ZhuzhginNA and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Incoming Registration", {
+// 	refresh(frm) {
+
+// 	},
+// });
